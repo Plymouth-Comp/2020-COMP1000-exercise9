eb36ab67abf404313d1ddc41b1dc36732caf2938
Swen E Gaudl <swen.gaudl@gmail.com>
2020-12-12 11:38:31 +0000

    Initial commit

---
+++ Exercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercise
+{
+    public class Program
+    {
+        public int OpenFile(string fileName)
+        { // opens a local file from a given path such as Exercise.Tests/files/text1.txt
+          // returns -2 if directory does not exist, -1 if file could not be opened; 0 if everything worked
+
+                return -1;
+        }
+
+        public string[] OpenFileAndReadLines(string fileName, int howMany)
+        { // opens a local file from a given path such as Exercise.Tests/files/text1.txt
+          // returns the first <howMany> lines from the given file or an empty string array if the file does not exist
+
+            return null;
+        }
+
+        public int PersistentFileOpen(string fileName)
+        { // opens a local file from a given path such as Exercise.Tests/files/text1.txt
+          // returns -2 if directory does not exist, -1 if file could not be opened; 0 if everything worked
+          // return 1 if the file is already open
+
+
+            return -1;
+        }
+
+        public int PersistentFileClose(string fileName)
+        { // closes a local file from a given path such as Exercise.Tests/files/text1.txt
+          // returns -1 if file could not be closed; 0 if everything worked
+
+            return -1;
+        }
+
+        public string[] PersistentFileRead(int numOfLines)
+        { // uses the last opened file and reads a given number of lines
+          // returns <numOfLines> lines from the given file or 
+          //  an empty string array if the file does not exist
+          // if called a second time it again reads from the beginning of the file
+
+
+            return new string[10];
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Exercise 7!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# 2020-COMP1000-exercises
+Template Repo for Assessment 1 of Plymouth COMP1000 Module
+
+To update to the current tests you will need to execute the `update.bat` file
+
+Additionally, to checkout the project you will need to run `git clone --recursive <repo>`
+
+If not specifically stated you are not allowed to use further advanced libraries. This includes the usage of LINQ.
